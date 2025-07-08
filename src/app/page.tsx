ae23d09084e65f61017dfcac1811e897fe1fa419
MIGUEL MORT <devmigueluicab@gmail.com>
2025-07-07 18:14:04 -0600

    refactor layout and sections for improved structure and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Header from "./components/header";
 import HeroSection from "@/app/components/hero-section";
 import ServicesSection from "@/app/components/services-section";
 import AboutSection from "@/app/components/about-section";
 import ProjectsSection from "@/app/components/projects-section";
 import CTASection from "@/app/components/cta-section";
-import Footer from "@/app/components/footer";
 import ExperienceSection from "@/app/components/experience-section";
 import ClientsSection from "@/app/components/clients-section";
 
